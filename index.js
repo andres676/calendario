60b709d01dd962f3906b1f18a4ad4fc2b82a3a42
Andres Ramos <r.rubio676@gmail.com>
2022-09-29 14:19:48 -0500

    clase04 completada

--- index.js
+++ index.js
@@ -9,13 +9,31 @@ const createTask = (e) => {
     task.classList.add("card")
     input.value = "";
 
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  task.innerHTML = content;
-  list.appendChild(task);
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerHTML = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+
+    const content = 
+            `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.appendChild(taskContent);
+    list.appendChild(task);
 };
  
 btn.addEventListener("click",createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+};
+
+const completeTask = (e) => {
+    const element = e.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+};
