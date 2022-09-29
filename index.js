3007f25a895e2f29eacbb3467a9b620932523c8f
Andres Ramos <r.rubio676@gmail.com>
2022-09-29 11:28:56 -0500

    clase02 completada

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("[data-form-btn]");
+
+const createTask = (e) => {
+    e.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+};
+ 
+btn.addEventListener("click",createTask);
